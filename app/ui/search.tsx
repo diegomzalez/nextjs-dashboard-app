6f83c1597fc736f5b861af70187044e6a91478e9
Diego Gonzalez <diegomzalez@hotmail.com>
2024-10-05 18:33:41 -0600

    fix: lint and build
    Fix build errors and lint warnings.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,9 +11,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams.toString());
     params.set("page", "1");
-    term ? params.set("query", term) : params.delete("query");
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
